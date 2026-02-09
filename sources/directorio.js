cf5969b2f5dfe95808c23ba593c2770d4812555f
Victor-Mtz2 <vicmartinez@uv.mx>
2026-02-09 09:49:16 -0600

    corrección de errores

--- sources/directorio.js
+++ sources/directorio.js
@@ -28,27 +28,31 @@ function directorio() {
         DGAbierta: true,
         titular: {
             nombre: "L.C. Rosa Aidé Villalobos Betancourt",
-            puesto: "Directora General",
-            telefono: "228 141 1053 Conmutador: 228 842 1700 ext. 11703",
+            puesto: "Directora General de Recursos Humanos",
+            telefono: "228 141 1053  Conmutador: 228 842 1700 ext. 11703",
+            mail: "rovillalobos@uv.mx"
             
         },
         subdirectores: [
             {
                 nombre: "Mtra. Liliana Ruiz Mendoza",
                 puesto: "Directora de Personal",
-                telefono: "228 842 1705 Conmutador: 228 842 1700 ext. 11705",
+                telefono: "228 842 1705  Conmutador: 228 842 1700 ext. 11705",
+                mail: "liruiz@uv.mx",
                 foto: "liliana.png"
             },
             {
                 nombre: "Mtra. Evangelina Murcia Villagómez",
                 puesto: "Directora de Nóminas",
-                telefono: "228 842 1727 Conmutador: 228 842 1700 ext. 11727",
+                telefono: "228 842 17277",
+                conmutador: "228 842 1727 ext. 11727",
                 foto: "evangelina.png"
             },
             {
                 nombre: "Mtra. Sandra Rodríguez José",
                 puesto: "Directora de Relaciones Laborales",
-                telefono: "228 842 1704 Conmutador: 228 842 1700 ext. 11704",
+                telefono: "228 842 1704",
+                conmutador: "228 842 1727 ext. 11704",
                 foto: "sandra.png"
             }
         ],
@@ -56,19 +60,19 @@ function directorio() {
             {
                 nombre: "Mtra. Laura Patricia Reyes Juárez",
                 puesto: "Administradora",
-                telefono: "228 842-27-00 Ext. 11195"
+                conmutador: "228 842-17-00 Ext. 11195"
                 // sin foto
             },
             {
                 nombre: "Lic. María Guadalupe Vázquez Castillo",
-                puesto: "Jefa del Departamento de Evaluación y Desarrollo de Personal",
-                telefono: "228 842-27-00 Ext. 11114"
+                puesto: "Jefa del Depto. de Evaluación y Desarrollo de Personal",
+                conmutador: "228 842-27-00 Ext. 11114"
                 // sin foto
             },
             {
                 nombre: "Mtro. Victor Alfonso Martínez Martínez",
                 puesto: "Jefe del Departamento de Asuntos Técnicos",
-                telefono: "228 842-27-00 Ext. 11100"
+                conmutador: "228 842-27-00 Ext. 11100"
                 // sin foto
             }
         ],
@@ -78,41 +82,42 @@ function directorio() {
                 abierto: false,
                 director: {
                     nombre: "Mtra. Liliana Ruiz Mendoza",
-                    puesto: "Directora",
-                    telefono: "228 842 1705 Conmutador: 228 842 1700 ext. 11705",
+                    puesto: "Directora de Personal",
+                    telefono: "228 842 1705",
+                    conmutador: "228 842 1700 ext. 11705",
                     foto: "laura.png"
                 },
                 jefaturas: [
                     {   
-                        Dirdep: "Departamento de Control de Personal Académico",
+                        Dirdep: "Jefe del Depto. de Control de Personal Académico",
                         nombre: "L.C. José Ladislao Ruíz Baizabal",
                         puesto: "Jefe de Departamento",
                         telefono: "228 200 2222",
                         foto: "carlos.png"
                     },
                     {
-                        Dirdep: "Departamento de Control de Personal Administrativo",
+                        Dirdep: "Jefe del Depto. de Control de Personal Administrativo",
                         nombre: "Mtro. Hugo Armenta Cuevas",
                         puesto: "Jefe de Departamento",
                         telefono: "228 200 3333"
                         // sin foto → usa default
                     },
                     {   
-                        Dirdep: "Departamento de Prestaciones Sociales",
+                        Dirdep: "Jefa del Departamento de Prestaciones Sociales",
                         nombre: "Mtra. Karla Henneyssy Fernández Hernández",
                         puesto: "Encargada de Departamento",
                         telefono: "228 200 2222",
                         foto: "carlos.png"
                     },
                     {   
-                        Dirdep: "Departamento de Gestión de Personal",
+                        Dirdep: "Jefa del Departamento de Gestión de Personal",
                         nombre: "Lic. Gabriela Guevara Portugal",
                         puesto: "Jefa de Departamento",
                         telefono: "228 200 2222",
                         foto: "carlos.png"
                     },
                     {   
-                        Dirdep: "Departamento de Archivo",
+                        Dirdep: "Jefe del Departamento de Archivo",
                         nombre: "Mtro. Edsel Ortiz Moreno",
                         puesto: "Jefe de Departamento",
                         telefono: "228 200 2222",
@@ -125,20 +130,20 @@ function directorio() {
                 abierto: false,
                 director: {
                     nombre: "Mtra. Evangelina Murcia Villagómez",
-                    puesto: "Directora",
+                    puesto: "Directora de Nóminas",
                     telefono: "228 842 1727 Conmutador: 228 842 1700 ext. 11727",
                     foto: "laura.png"
                 },
                 jefaturas: [
                     {
-                        Dirdep: "Departamento de Validación y Control",
+                        Dirdep: "Jefa del Departamento de Validación y Control",
                         nombre: "Mtra. Tania Guadalupe Gutiérrez Salvador",
                         puesto: "Jefa de Departamento",
                         telefono: "228 200 3333"
                         // sin foto → usa default
                     },
                     {   
-                        Dirdep: "Departamento de Registro",
+                        Dirdep: "Jefe del Departamento de Registro",
                         nombre: "L.C. Ommar Pérez Rojas",
                         puesto: "Jefe de Departamento",
                         telefono: "228 200 2222",
@@ -151,20 +156,20 @@ function directorio() {
                 abierto: false,
                 director: {
                     nombre: "Mtra. Sandra Rodriguez José",
-                    puesto: "Directora",
+                    puesto: "Directora de Relaciones Laborales",
                     telefono: "228 842 1704 Conmutador: 228 842 1700 ext. 11704",
                     foto: "laura.png"
                 },
                 jefaturas: [
                     {
-                        Dirdep: "Departamento de Asesoría y Procedimientos Laborales",
+                        Dirdep: "Jefe del Depto. de Asesoría y Procedimientos Laborales",
                         nombre: "Lic. José Julián Perea Contreras",
                         puesto: "Jefe de Departamento",
                         telefono: "228 200 3333"
                         // sin foto → usa default
                     },
                     {   
-                        Dirdep: "Departamento de Juicios Laborales",
+                        Dirdep: "Jefa del Depto. Departamento de Juicios Laborales",
                         nombre: "Mtra. Rita Robles Hernández",
                         puesto: "Jefe de Departamento",
                         telefono: "228 200 2222",
