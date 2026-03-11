187f090e2907db23c1a0d9217a218110c3fce4d4
Victor-Mtz2 <vicmartinez@uv.mx>
2026-03-11 11:27:30 -0600

    sintesis Curricular

---
+++ sources/datosSintesis.js
@@ -0,0 +1,8 @@
+const SINTESIS = [
+    {nopersonal: "30974", nombre: "Victor Alfonso Martínez Martínez", titulo: "Licenciatura en Sistemas Computacionales Administrativos", 
+     posgrados: "Maestría en Sistemas Computacionales Administrativos", cargos: "2006-2024 Desarrollador de Sistemas en la DGTI",
+     otras: "Aministrativas"},
+    {nopersonal: "30974", nombre: "Liliana Ruiz Mendoza", titulo: "Licenciatura en Estadística", 
+     posgrados: "Maestría en Educación", cargos: "2022-2025 Secretaria Particular SAF",
+     otras: "Funcionarias,Aministrativas"},
+];
